2b9b3bb5e095ac3b7e1b913bd2629b1f6a416afe
NandhiniR123 <50901390+NandhiniR123@users.noreply.github.com>
2019-05-23 15:57:47 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=int(input(" A value"))
+b=int(input(" B value"))
+c=int(input(" C value"))
+if((b<=a)and(c<=a)):
+  print("A is greater")
+elif((a<=b)and(c<=b)):
+  print("B is greater")
+else:
+  print("C is greater")
